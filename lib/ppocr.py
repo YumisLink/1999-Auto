1070f427a30423846f08cfd357a8752c63047214
DoyoDia <79502694+DoyoDia@users.noreply.github.com>
2023-06-14 22:14:38 +0800

    更新ppocr api

--- lib/ppocr.py
+++ lib/ppocr.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from lib.OCR.api import GetOcrApi
 #config_path是相对于PaddleOCR-json.exe的路径
 argument_cn = {'config_path': "models/config_chinese.txt"}
@@ -14,9 +15,19 @@ def ocr_cn(target):
     res = ocr.run(target_path)
     return res
 
-def ocr_xy(target):
-    
-    print('还没写')
+def ocr_xy(target,text='',cn=True):
+    """
+    识别target图片中包含text的区域，返回中心坐标
+    :param res:返回的字典.
+    :param text:预期文本.
+    :param cn:是否使用中文ocr.
+    :Return: 中心坐标和匹配度.
+    """
+    if cn:
+        res=match_text_area(ocr_cn(target), text)
+    else:
+        res=match_text_area(ocr_en(target), text)
+    return res
 
 
 def ocr_en(target):
@@ -25,3 +36,34 @@ def ocr_en(target):
     res = ocr2.run(target_path)
 
     return res
+
+def match_text_area(res, text):
+    """
+    在返回中筛选出包含text的区域，如果有多个区域，选择最接近中心的区域 返回其中心坐标和相似度
+    :param res:返回的字典.
+    :param text:预期文本.
+    :Return: 中心坐标和相似度.
+    """
+    if res['code'] != 100:
+        print(res)
+        return None, None
+    match_areas = []
+    similarities = []
+    for item in res['data']:
+        if text in item['text']:
+            match_areas.append(item['box'])
+            similarities.append(item['score'])
+    if len(match_areas) == 0:
+        return None, None
+    if len(match_areas) == 1:
+        center = np.mean(match_areas[0], axis=0)
+        return center.tolist(), similarities[0]
+    # 如果有多个匹配区域，选择最接近中心的区域
+    centers = []
+    for area in match_areas:
+        center = np.mean(area, axis=0)
+        centers.append(center)
+    centers = np.array(centers)
+    distances = np.linalg.norm(centers - np.array(res['image_size']) / 2, axis=1)
+    min_idx = np.argmin(distances)
+    return centers[min_idx].tolist(), similarities[min_idx]
