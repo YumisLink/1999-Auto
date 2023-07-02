f2beb5bef73ff751261f8c09d90260ceeabe6ef4
DoyoDia <79502694+DoyoDia@users.noreply.github.com>
2023-07-02 19:51:08 +0800

    添加自动领点唱机的函数

---
+++ plugins/pass.py
@@ -0,0 +1,65 @@
+#领取通行证（点唱机）奖励
+import time
+import lib.find as f
+import lib.adb_command as adb
+import plugins.path as path
+
+
+IMG_PASS = 'imgs/pass/pass'
+IMG_CLAIM_ALL='imgs/pass/claim_all'
+IMG_REDPOINT='imgs/pass/redpoint'
+IMG_CLAIM='imgs/pass/claim'
+IMG_UPGARDE='imgs/pass/upgrade'
+
+def pass_start():
+    """领取点唱机奖励"""
+    path.to_menu()
+    adb.touch([270,86])
+    time.sleep(1)
+
+    res = f.cut_find_html('imgs/pass/redpoint',1524,84,1555,50)
+    if res is not None:
+        print('开始领取')
+        #读碟
+        adb.touch([1417,98])
+        time.sleep(0.3)
+    else:
+        print('没有奖励可领取')
+        return
+    #日记
+    claim_all()
+    claim()
+    #周报
+    adb.touch([1024,283])
+    claim_all()
+    claim()
+    #公约
+    adb.touch([1383,483])
+    claim_all()
+    claim()
+
+def claim():
+    while True:
+        res=f.cut_find_html(IMG_CLAIM,1410,324,1546,784)
+        if res is not None:
+            adb.touch(res)
+            time.sleep(0.3)
+            db_upgarde()
+        else:
+            return False
+def claim_all():
+    res=f.cut_find_html(IMG_CLAIM_ALL,1194,777,1598,897)
+    if res is not None:
+        adb.touch(res)
+        time.sleep(0.3)
+        db_upgarde()
+        return True
+    else:
+        return False
+
+def db_upgarde():
+    res=f.cut_find_html(IMG_UPGARDE,695,483,881,534)
+    if res is not None:
+        adb.touch([928,575])
+        time.sleep(0.3)
+        return True
