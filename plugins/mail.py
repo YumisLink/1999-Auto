e4c866a49c1e3e578d7bf3ab0ab8754864ddbae8
DoyoDia <79502694+DoyoDia@users.noreply.github.com>
2023-07-06 00:24:16 +0800

    删除了铸币脑袋创建的错误

--- plugins/mail.py
+++ plugins/mail.py
@@ -2,9 +2,10 @@ import time
 import lib.adb_command as adb
 import plugins.path as path
 
-path.to_menu()
-adb.touch([98,230])
-time.sleep(1)
-adb.touch([263,770])
-time.sleep(1)
-path.to_menu()
+def mail_start():
+    path.to_menu()
+    adb.touch([98,230])
+    time.sleep(1)
+    adb.touch([263,770])
+    time.sleep(1)
+    path.to_menu()
